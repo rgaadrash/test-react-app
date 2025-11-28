f841d6ef29c3ce0724d4b6d7d29d19fdd2c4b26a
rgaadrash <aadrashmehta@revyrieglobal.com>
2025-11-29 01:07:11 +0530

    updated code

--- src/apollo-client.js
+++ src/apollo-client.js
@@ -2,7 +2,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:4000/graphql', // adjust to your GraphQL endpoint
+    // uri: import.meta.env.VITE_API_URL || 'http://localhost:4000/graphql', // adjust to your GraphQL endpoint
+    uri: import.meta.env.VITE_API_URL, // adjust to your GraphQL endpoint
   }),
   cache: new InMemoryCache(),
 });
